fc6d3cf4c820067b906657f27bea110094699b25
wells99 <9.9wellington9.9@gmail.com>
2025-06-05 00:24:21 -0300

    services - update

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,19 +4,19 @@ const Services = () => {
         {id:1,
             img:img2,
             name:"Expresso",
-            description:"lorem ipsum dolor site amet lorem ipsum dolor site amet lorem ipsum dolor site amet",
+            description:"um cuidado artesanal: grãos frescos, moídos na hora, transformam-se em pó finíssimo. Compactado com precisão, ele encontra a água quente sob alta pressão, que, em segundos, extrai a alma do café. O resultado é um néctar concentrado, de sabor intenso e uma crema aveludada, essência pura em cada gota.",
             aosDelay:"100"
         },
         {id:2,
             img:img2,
             name:"Americano",
-            description:"lorem ipsum dolor site amet lorem ipsum dolor site amet lorem ipsum dolor site amet",
+            description:"a essência do espresso com a leveza da água quente. Cuidadosamente preparado, o café concentrado recebe a água na temperatura ideal, em um fluxo suave que realça seus sabores. É uma bebida pura e convidativa, onde a força do café encontra a delicadeza da diluição, entregando uma experiência saborosa e equilibrada.",
             aosDelay:"300"
         },
         {id:3,
             img:img2,
             name:"Cappucino",
-            description:"lorem ipsum dolor site m ipsum dol m ipsum m ipsum dol m ipsum dol amet lorem ipsum dolor s dol amet lorem ipsum dolor site amet lorem ipsum dolor site amet",
+            description:"Um cappuccino é a fusão de um espresso intenso com a suavidade do leite vaporizado, coroado por uma espuma aveludada. Com precisão, o café, rico e aromático, recebe o leite transformado em seda líquida, criando camadas que dançam harmoniosamente. O resultado é uma bebida que conforta, unindo força e delicadeza em cada gole.",
             aosDelay:"500"
         },
     ]
@@ -27,7 +27,7 @@ const Services = () => {
             <div className="container overflow-hidden">
                 <div data-aos="fade-up" className="text-center mb-20 ">
                     <h1 className="text-4xl font-bold !font-cursive text-gray-800">
-                        Best Coffe For You
+                       Campeões de vendas
                     </h1>
                 </div>
 
@@ -41,7 +41,7 @@ const Services = () => {
                             data-aos-delay={data.aosDelay}
                             key={index} 
                             className=" rounded-2xl bg-white hover:bg-primary hover:text-white 
-                            shadow-xl  duration-200 max-w-[300px] group relative ">
+                            shadow-xl  duration-200 max-w-[300px] group relative cursor-pointer">
                                 {/* imagem */}
                                 <div className="h-[122px] ">
                                     <img 
